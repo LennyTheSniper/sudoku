a690eca9156c0d22baf8b89e35e349fa9069427e
Kevin JOSEPH <kevin.joseph@ens.uvsq.fr>
2023-03-24 18:59:04 +0100

    new bind method

--- sudoku.py
+++ sudoku.py
@@ -4,6 +4,7 @@ import random as rd
 import numpy as np
 import copy as cp
 
+
 # variables
 COTE = 650
 COTE3 = COTE//3
@@ -27,7 +28,6 @@ test2 = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
  [1, 2, 3, 4, 5, 6, 7, 8, 9],
  [1, 2, 3, 4, 5, 6, 7, 8, 9],
  [1, 2, 3, 4, 5, 6, 7, 8, 9]]
-
 
 # fonctions et GUI
 def grille():
@@ -60,22 +60,82 @@ def clic(event):
     print(item_id)
     canvas.itemconfig(item_id, fill="grey")
     item_id += 85
-    canvas.itemconfig(item_id, text="")
+    canvas.itemconfig(item_id, text="en attente")
     print(item_id)
     return item_id
 
 
-def entrer(event):
+def un(event):
     global item_id
-    #print(type(saisi.get()))
-    canvas.itemconfig(item_id, text=saisi.get())
-    #if le chiffre est au bon endroit:
-    #    item_id -= 85
-    #    canvas.itemconfig(item_id, fill="white")
-    #else:
-    #    canvas.itemconfig(item_id, fill="red")
+    canvas.itemconfig(item_id, text="1")
+    item_id -= 85
+    canvas.itemconfig(item_id, fill="white")
     item_id = 0
+    return item_id
 
+def deux(event):
+    global item_id
+    canvas.itemconfig(item_id, text="2")
+    item_id -= 85
+    canvas.itemconfig(item_id, fill="white")
+    item_id = 0
+    return item_id
+
+def trois(event):
+    global item_id
+    canvas.itemconfig(item_id, text="3")
+    item_id -= 85
+    canvas.itemconfig(item_id, fill="white")
+    item_id = 0
+    return item_id
+
+def quatre(event):
+    global item_id
+    canvas.itemconfig(item_id, text="4")
+    item_id -= 85
+    canvas.itemconfig(item_id, fill="white")
+    item_id = 0
+    return item_id
+
+def cinq(event):
+    global item_id
+    canvas.itemconfig(item_id, text="5")
+    item_id -= 85
+    canvas.itemconfig(item_id, fill="white")
+    item_id = 0
+    return item_id
+
+def six(event):
+    global item_id
+    canvas.itemconfig(item_id, text="6")
+    item_id -= 85
+    canvas.itemconfig(item_id, fill="white")
+    item_id = 0
+    return item_id
+
+def sept(event):
+    global item_id
+    canvas.itemconfig(item_id, text="7")
+    item_id -= 85
+    canvas.itemconfig(item_id, fill="white")
+    item_id = 0
+    return item_id
+
+def huit(event):
+    global item_id
+    canvas.itemconfig(item_id, text="8")
+    item_id -= 85
+    canvas.itemconfig(item_id, fill="white")
+    item_id = 0
+    return item_id
+
+def neuf(event):
+    global item_id
+    canvas.itemconfig(item_id, text="9")
+    item_id -= 85
+    canvas.itemconfig(item_id, fill="white")
+    item_id = 0
+    return item_id
 
 def verifligne(liste):
     for i in range(len(liste)):
@@ -147,12 +207,17 @@ def lirecarre(liste):
 racine = tk.Tk()
 racine.title("SUDOKU")
 canvas = tk.Canvas(racine, height=COTE, width=COTE, bg="white")
-tk.Label(racine, text="Entrez le chiffre juste en dessous").grid(row=0, column=2)
-saisi = tk.Entry(racine, width=1)
 canvas.grid(row=0, column=1, rowspan=10)
-saisi.grid(row=1, column=2)
 grille()
 texte()
 canvas.bind('<ButtonRelease-1>', clic)
-racine.bind('<Return>', entrer)
+racine.bind('<F1>', un)
+racine.bind('<F2>', deux)
+racine.bind('<F3>', trois)
+racine.bind('<F4>', quatre)
+racine.bind('<F5>', cinq)
+racine.bind('<F6>', six)
+racine.bind('<F7>', sept)
+racine.bind('<F8>', huit)
+racine.bind('<F9>', neuf)
 racine.mainloop()
